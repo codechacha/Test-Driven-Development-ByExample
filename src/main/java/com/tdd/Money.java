9e0485b8c6ebacad7a510dfbc4de66802f4fb684
Jeongsik Mun <farfs.dev@gmail.com>
2019-02-12 22:02:54 +0900

    page 92

--- src/main/java/com/tdd/Money.java
+++ src/main/java/com/tdd/Money.java
@@ -1,11 +1,21 @@
 package com.tdd;
 
-public class Money {
+abstract class Money {
     protected int amount;
+
+    static public Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static public Money franc(int amount) {
+        return new Franc(amount);
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
                 && getClass().equals(money.getClass());
     }
+
+    abstract Money times(int multiplier);
 }
